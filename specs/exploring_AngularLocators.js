fb3eac6c94d2e56ec6b081ab65c1da04f5065a79
ImRaNM-001 <imu163@gmail.com>
2020-02-10 11:12:39 +0530

    commit

--- specs/exploring_AngularLocators.js
+++ specs/exploring_AngularLocators.js
@@ -2,25 +2,24 @@ describe('exploring Angular Locators', () =>{
 
     let result;
 
-    beforeAll(function() {
+    beforeAll( () =>{
         browser.waitForAngularEnabled(false);
     } );
 
-    beforeEach(function() {
+    beforeEach( () =>{
         browser.get('https://juliemr.github.io/protractor-demo/');    
         result = element(by.className('ng-binding') );
-        // result = element(by.cssContainingText('.ng-binding', 4) )
     } );   
 
     // TC1: identifying locators and performing the multiplication operation
-    it('perform multiplication of two numbers', () => {
+    it('perform multiplication of two numbers', () =>{
         const firstNo = element(by.model('first') );
-        const divOperator = element(by.model('operator') ).$('[value="MULTIPLICATION"]' );           // selecting value inside model operator using $
+        const mulOperator = element(by.model('operator') ).$('[value="MULTIPLICATION"]' );           // selecting value inside model operator using $
         const secondNo = element(by.model('second') );
         const goButton = $('[ng-click="doAddition()"]');            // using $ to store css
 
         firstNo.sendKeys(10);                       // no quotes required simple number also accepted
-        divOperator.click();
+        mulOperator.click();
         secondNo.sendKeys(2);
         goButton.click();
         
--- specs/print_in_Console_&_verifyResult.js
+++ specs/print_in_Console_&_verifyResult.js
@@ -2,18 +2,21 @@ describe('print & verify a result', () =>{
 
     let result;
 
-    beforeAll(function() {
+    beforeAll( () =>{
         browser.waitForAngularEnabled(false);
     } );
 
-    beforeEach(function() {
+    beforeEach( () => {
         browser.get('https://juliemr.github.io/protractor-demo/');    
         result = element(by.className('ng-binding') );
+
         // result = element(by.cssContainingText('.ng-binding', 4) )
     } );   
 
-    // TC1: performing division operation
+    // TC1: performing division operation, print the result in console & verifying the result using expect.
     it('perform division of two numbers', () => {
+        browser.get('https://juliemr.github.io/protractor-demo/'); 
+
         const firstNo = element(by.model('first') );
         const divOperator = element(by.model('operator') ).$('[value="DIVISION"]' );           // selecting value inside model operator using $
         const secondNo = element(by.model('second') );
@@ -23,27 +26,21 @@ describe('print & verify a result', () =>{
         divOperator.click();
         secondNo.sendKeys(5);
         goButton.click();
-        browser.sleep(6000);
+        
+        browser.sleep(3000);
+        
+    //    console.log('The division result is: ' + (result.getText() ));      // simple getText() fn will return promises and print [object Object],
+
+
+        // hence in order to get correct value we have to resolve that promise
+        result.getText().then( (value) => {
+            console.log('The division result is: ' + value);
+        } ); 
+
+        expect(result.getText() ).toEqual('4');
     } );
 
-    // TC2: print the result in console
-    it('print the div result in console', async function() {
-    //    console.log('The division result is: ' + (result.getText() ));               // simple getText() fn will return promises and print [object Object], hence in order to get correct value we have to resolve that promise
-       
-       await result.getText().then( (value) => {
-           console.log('The division result is: ' + value);
-       } );       
-        // console.log('The division result is: ' + result.getAttribute('value') );
-    } );
-    
-   // TC3: verifying the result using expect
-   it('verifying the result',  function() {
-         expect(result.getText() ).toEqual(4);
-        // browser.sleep(6000);
-   } );
-
-
-
-
+     
+ // console.log('The division result is: ' + result.getAttribute('value') );
 
 } );
